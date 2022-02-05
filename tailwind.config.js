948bf43e6ee7ce32b968650d5fef5835c44919c1
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2022-02-05 19:13:57 -0300

    avances

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,13 @@ module.exports = {
         'do-blue-dark': '#080c2d',
         'do-blue-medium-dark': '#1d274c',
         'do-blue-light': 'rgb(20,86,255)',
-        'do-blue-medium': 'rgb(0,105,255)'
+        'do-blue-medium': 'rgb(0,105,255)',
+        'nil-blue-dark': '#0c2c44',
+        'nil-blue-medium': '#2c4c5b',
+        'nil-yellow-light': '#fcdc84',
+        'nil-yellow-dark': '#e09d10',
+        'nil-gray': '#b3bdc4',
+        'nil-bronce': '#ac9c74',
       },
       boxShadow: {
         'input': '0 4px 1px 0 rgba(0, 0 ,0 ,0.1)',
@@ -23,5 +29,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
